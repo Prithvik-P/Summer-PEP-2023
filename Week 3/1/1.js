463b9684502a8433f305351e111be3811a91a5cd
PRITHVIK P <prithvikprabodh9@gmail.com>
2023-07-01 21:07:52 +0530

    1-7-2023-2

---
+++ Week 3/1/1.js
@@ -0,0 +1,10 @@
+const user = {
+    Fullname : "Prantosh" ,
+    Email : "prantosh@gmail.com" ,
+    mobile : {
+        code : "+" + 91 ,
+        Number : 9221361232
+    }
+};
+
+console.log(user);
